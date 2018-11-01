498ac8fb4eedd25d4811d3f90362058a147b92eb
INTRA\asela.illayapparachc <asela.illayapparachc@rakuten.com>
2018-11-01 15:38:03 +0800

    groovy intellij project

---
+++ hello-intellij/src/com/asela/HelloWorld.groovy
@@ -0,0 +1,8 @@
+package com.asela
+
+/**
+ * Created by asela.illayapparachc on 1/11/2018.
+ */
+class HelloWorld {
+
+}
---
+++ hello-intellij/src/com/asela/hello.groovy
@@ -0,0 +1,6 @@
+package com.asela
+
+/**
+ * Created by asela.illayapparachc on 1/11/2018.
+ */
+print "hello, world"
